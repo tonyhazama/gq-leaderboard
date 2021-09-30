d7c0e39ad180fec0712e7d3d040a99063c614a33
Tony Hazama <tonyhazama4721@gmail.com>
2021-09-30 21:47:24 +0700

    added animation

--- src/App.js
+++ src/App.js
@@ -9,25 +9,29 @@ function App() {
 
   const generateContestant = (contestant = {}) => {
     return (
-      <div style={{
+      <div 
+      style={{
         display: "flex",
         alignItems: 'center',
         flexDirection: "column",
-        padding: "0px 1em"
+        padding: "0px 1.5em 3em",
       }}>
         <div style={{marginBottom: "0.75em", whiteSpace: 'break-spaces'}}>
           {'# ' + contestant.number + ' \n' + contestant.name}
         </div>
-        <div style={{
-        display: "flex",
-        alignItems: 'center',
-        flexDirection: "column"
-      }}>
+        <div 
+        className="portrait"
+        style={{
+          display: "flex",
+          alignItems: 'center',
+          flexDirection: "column",
+          animationDelay: `${(contestant.number%3) % (contestant.number / 3)}s`
+        }}>
           <div style={{width: '120px', height: '170px', overflow: 'hidden'}}>
             <img style={{objectFit: 'cover', objectPosition: 'center', width: '100%', height: '100%'}} src={process.env.PUBLIC_URL + `/${contestant.number}.jpg`}/>
           </div>
           <div style={{width: '140px', position: 'absolute'}}>
-            <img style={{width: '100%'}} src={process.env.PUBLIC_URL + '/portrait.png'} />
+            <img style={{width: '100%', boxShadow: '0px 0px 20px rgba(0, 0, 0, .4)'}} src={process.env.PUBLIC_URL + '/portrait.png'} />
           </div>
         </div>
         <div style={{
@@ -73,12 +77,24 @@ function App() {
         backgroundImage: `url('${process.env.PUBLIC_URL}/bg.jpg')`,
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        width: '100%',
       }}
     >
-      <div>
+      <div
+        style={{
+          width: '100%',
+          overflow: 'hidden'
+        }}
+      >
         <div style={{marginBottom: '1em', fontSize: '3em'}}>Contestant</div>
-        <div style={{display: 'flex'}}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            flexWrap: 'wrap'
+          }}
+        >
           {scores.map((score, key) => {
             score.number = key + 1;
             return generateContestant(score);
